14bfa602e28c3c0bf1487a734ff194664efd657e
Robinson Fonseca <robinsonfonseca@MacBook-Air-de-Robinson.local>
2021-04-10 11:34:21 -0500

    definición de variables

---
+++ 1.variables/variable.dart
@@ -0,0 +1,12 @@
+void main() {
+  String nombre = "Juan";
+  int edad = 30;
+  String telefono = "3208598245";
+  int? variableAceptanull = null;
+
+  final String segundoapellido = "Herrera";
+
+  var apellido;
+
+  print("Nombre: $nombre, edad: $edad, Telefono: $telefono");
+}
