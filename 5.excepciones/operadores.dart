ebdce609d46dcb8c820f91f3d248a0cd7b344d49
Robinson Fonseca <39870129+rfonseca7@users.noreply.github.com>
2021-05-19 10:33:43 -0500

    Actualización excepciones

--- 5.excepciones/operadores.dart
+++ 5.excepciones/operadores.dart
@@ -1,25 +1,27 @@
 void main() {
   try {
-    print(obtenerNombre("juan"));
+  //  print(obtenerNombre("Juan"));
+    print(obtenerNombreDos("Robinson"));
   } on FormatException catch (e) {
     print("Se produjo un error de tipo FormatException");
   } catch (e) {
-    print("Se produjo un error en la aplicacion");
+    print(
+        "Se produjo un error sin TIPO en la aplicacion, comunicar con el admin");
   } finally {
-    print("este es el error finally");
+    print("este es el finally");
   }
 }
 
 String obtenerNombre(String? nombre) {
-  if (nombre == 'juan') {
+  if (nombre == 'Juan') {
     throw FormatException("El nombre no puede ser $nombre");
   }
-  return "mi nombre es $nombre";
+  return "Mi nombre es $nombre";
 }
 
 String obtenerNombreDos(String? nombre) {
-  if (nombre == 'juan') {
+  if (nombre == 'Juan') {
     throw ("El nombre no puede ser $nombre");
   }
-  return "mi nombre es $nombre";
+  return "Mi nombre es $nombre";
 }
